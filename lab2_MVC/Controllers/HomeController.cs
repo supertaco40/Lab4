0b00c543228fd6791901885ac11d71237e3a5064
supertaco40 <106879476+supertaco40@users.noreply.github.com>
2022-06-05 22:00:24 +0600

    init

---
+++ lab2_MVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab2_MVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public async Task<string> GetData()
+    {
+        var clientHandler = new HttpClientHandler();
+        clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+        var client = new HttpClient(clientHandler);
+
+        client.BaseAddress = new Uri("https://localhost:7143/City");
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        var response = await client.GetAsync("/City");
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+}
